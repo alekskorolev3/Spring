c1d4b202d66bfce2d1c8606f4fe558091bbc43f2
alekskorolev3 <alekskorolev3@gmail.com>
2021-03-26 11:38:25 +0300

    Initial commit

--- book/src/main/resources/static/js/app.js
+++ book/src/main/resources/static/js/app.js
@@ -1,6 +1,25 @@
 
 //import { Button } from './button.js';
-class Button extends React.Component
+var countOfAddClicks = 0;
+var countOfEditClicks = 0;
+class AddBlock extends React.Component
+{
+    render()
+    {
+        return(
+            <div id="addBook">
+                <form method = "post" action="add">
+                    <p><input type = "text" name = "name" placeholder="Введите название" /></p>
+                    <p><input type = "text" name = "genre" placeholder="Введите жанр" /></p>
+                    <p><input type = "text" name = "author" placeholder="Введите автора" /></p>
+                    <button type="submit">Добавить</button>
+                    <button type="cancel">Отмена</button>
+                </form>
+            </div>
+        );
+    }
+}
+class AButton extends React.Component
 {
     render()
     {
@@ -9,17 +28,91 @@ class Button extends React.Component
         );
     }
 }
+
 class AddButton extends React.Component {
 
     handleClick()
     {
-        alert("Hello");
+        if (countOfAddClicks % 2 === 0)
+        {
+            ReactDOM.render(
+                <AddBlock />,
+                document.getElementById('add')
+            );
+        }
+        else
+        {
+            ReactDOM.render(
+                <div></div>,
+                document.getElementById('add')
+            );
+        }
+        countOfAddClicks = countOfAddClicks + 1;
+
     }
     render() {
-        return <Button onClick={this.handleClick}/>;
+        return <AButton onClick={this.handleClick}/>;
+    }
+}
+
+class EditBlock extends React.Component
+{
+    render()
+    {
+        return(
+            <div>
+                <form method="post" action="edit">
+                    <p><input type = "text" name = "name" placeholder="Введите название"/></p>
+                    <p><input type = "text" name = "genre" placeholder="Введите жанр"/></p>
+                    <p><input type = "text" name = "author" placeholder="Введите автора"/></p>
+                    <button type="submit" name="edit">Редактировать</button>
+                    <button type="submit" name="remove">Удалить</button>
+                </form>
+
+            </div>
+        );
+    }
+}
+
+class EButton extends React.Component
+{
+    render()
+    {
+        return(
+            <button onClick={this.props.onClick}>Редакт./Удалить</button>
+        );
+    }
+}
+
+class EditButton extends React.Component {
+
+    handleClick()
+    {
+        if (countOfEditClicks % 2 === 0)
+        {
+            ReactDOM.render(
+                <EditBlock />,
+                document.getElementById('add')
+            );
+        }
+        else
+        {
+            ReactDOM.render(
+                <div></div>,
+                document.getElementById('add')
+            );
+        }
+        countOfEditClicks = countOfEditClicks + 1;
+    }
+    render() {
+        return <EButton onClick={this.handleClick}/>;
     }
 }
 ReactDOM.render(
-    <AddButton/>,
+    <AddButton />,
     document.getElementById('root')
+);
+ReactDOM.render(
+    <EditButton />,
+    document.getElementById('edit')
 );
